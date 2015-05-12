b1b1ef903187e7c309b66064b9b7789ffc32ce29
JonnathanMc <jonnathan013@gmail.com>
2015-05-12 14:44:52 -0300

    Primeira versão

---
+++ Demon Attack/Source.cpp
@@ -0,0 +1,22 @@
+using namespace System;
+
+#define ESTADO_JOGO_MENU 0
+#define ESTADO_JOGO_GAMEPLAY 1
+#define ESTADO_JOGO_GAMEOVER 2
+
+int main()
+{
+	const Char JOGADOR_GRAFICO = (Char)9608;
+
+	Console::SetWindowSize(115, 40);
+	Console::SetBufferSize(115, 40);
+
+	for (;;){
+		
+
+
+
+	}
+
+	return 0;
+}
